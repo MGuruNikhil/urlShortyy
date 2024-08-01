bc97f1ce570bc963ea84b860f4904cd460bec6c3
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-08-01 19:08:04 +0530

    okay

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const myUrl = "http://127.0.0.1:5555/";
-// export const myUrl = "https://url-shortyy.vercel.app"
+// export const myUrl = "http://127.0.0.1:5555/";
+export const myUrl = "https://url-shortyy.vercel.app";
