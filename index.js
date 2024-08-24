6528a9e1ea2db199f6188995a269a369b2736148
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-08-24 23:39:25 +0530

    responsive and favicon

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import { fbApp } from "./fbConfig.js";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
